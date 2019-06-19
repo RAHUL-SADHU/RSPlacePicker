1b18f7adcabfed4a7b507e4802e81167a1b7c40d
Sanjay Prajapati <spshaan22@gmail.com>
2019-06-19 16:02:04 +0530

    Store Address field in LocationModel, Update gradle build version 3.4.1 instead of 3.4.0, Update git ignore file to ignore idea files

--- RSLibrary/src/main/java/com/app/rspicker/LocationModel.kt
+++ RSLibrary/src/main/java/com/app/rspicker/LocationModel.kt
@@ -5,7 +5,8 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class LocationModel(
-        val latitude: Double,
-        val longitude: Double,
-        val mapImage: String
+    val address: String,
+    val latitude: Double,
+    val longitude: Double,
+    val mapImage: String
 ) : Parcelable
